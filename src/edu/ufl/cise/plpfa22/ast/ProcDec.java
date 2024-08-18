6abcb5f8b1f12d2b5e8bd31952abab44c3149c58
Tre' Jeter <33637310+tjeter@users.noreply.github.com>
2024-08-18 01:33:37 -0400

    Add files via upload

---
+++ src/edu/ufl/cise/plpfa22/ast/ProcDec.java
@@ -0,0 +1,45 @@
+/**  This code is provided for solely for use of students in the course COP5556 Programming Language Principles at the 
+ * University of Florida during the Fall Semester 2022 as part of the course project.  No other use is authorized. 
+ */
+
+package edu.ufl.cise.plpfa22.ast;
+
+import edu.ufl.cise.plpfa22.IToken;
+import edu.ufl.cise.plpfa22.PLPException;
+
+public class ProcDec extends Declaration {
+
+	public final IToken ident;
+	public final Block block;
+	private String cn;
+	
+
+	public ProcDec(IToken firstToken, IToken name, Block body) {
+		super(firstToken);
+		this.ident = name;
+		this.block = body;
+	}
+
+
+	@Override
+	public Object visit(ASTVisitor v, Object arg) throws PLPException {
+		return v.visitProcedure(this, arg);
+	}
+
+	@Override
+	public String toString() {
+		return "ProcDec [" + (ident != null ? "ident=" + ident + ", " : "") + (block != null ? "block=" + block : "") + "]";
+	}
+
+
+	public void setClassName(String cn) {
+		this.cn = cn;
+	}
+
+	public String getClassName() {
+		return this.cn;
+	}
+	
+	
+
+}
